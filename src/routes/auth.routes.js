2065177eeab21ff3842b84abc3a8834bd8d3930b
Misbah Khan <misbahkhan201678@gmail.com>
2026-08-03 10:49:08 +0530

    token blacklisting ,login+register

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -20,4 +20,5 @@ authRouter.post("/register", authController.registerUserController)
  */
 authRouter.post("/login",authController.loginUserController)
 
+authRouter.post("/logout", authController.logoutUserController                        )
 module.exports= authRouter
