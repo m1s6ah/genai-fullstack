f68f87fb09c7f071d5a1830845b1e8c9ae63f648
Misbah Khan <misbahkhan201678@gmail.com>
2026-08-04 10:56:34 +0530

    added cookie parser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const cookieParser = require('cookie-parser')
 
 const app = express()// server initiate krna , server ka instance create krna
 //middlewares or routes create krna and use krna yeh app.js ka kaam hai
 
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
