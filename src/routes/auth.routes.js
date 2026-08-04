462033f9893bb5816c46d964556015592b9f4dde
Misbah Khan <misbahkhan201678@gmail.com>
2026-08-04 10:57:19 +0530

    get route for getme to check blacklisted token

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const{Router}= require('express')
 const authController = require("../controllers/auth.controller")
-
+const authMiddleware = require("../middlewares/auth.middleware")
 const authRouter = Router()
+
 /**
  * @route POST /api/auth/register
  * @description register a new user
@@ -20,5 +21,22 @@ authRouter.post("/register", authController.registerUserController)
  */
 authRouter.post("/login",authController.loginUserController)
 
-authRouter.post("/logout", authController.logoutUserController                        )
+/**
+ * @route GET /api/auth/logout
+ * @description logout user and blacklist the token
+ * @access Public 
+ * 
+ */
+authRouter.get("/logout", authController.logoutUserController )
+
+
+/***
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
+ */
+authRouter.get("/get-me", authMiddleware.authUser, authController.getMeController)
+
+
+
 module.exports= authRouter
