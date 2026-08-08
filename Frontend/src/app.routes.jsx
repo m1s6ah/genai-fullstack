d890ae36ed335203cf7c220e978cfc853298afa4
Misbah Khan <misbahkhan201678@gmail.com>
2026-08-08 10:46:17 +0530

    imports

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,15 +1,23 @@
-import {createBrowserRouter} from "react-router-dom"
+import {createBrowserRouter, Navigate} from "react-router-dom"
 import Login from "./features/auth/pages/login"
 import Register from "./features/auth/pages/register"
 
 
 export const router = createBrowserRouter([
     {
+        path: "/",
+        element: <Navigate to="/register" replace />
+    },
+    {
         path: "/login",
-        element: <Login />//component render krna hai
+        element: <Login />
     },
     {   
         path: "/register",
         element: <Register />
+    },
+    {
+        path: "*",
+        element: <Navigate to="/register" replace />
     }
 ])
